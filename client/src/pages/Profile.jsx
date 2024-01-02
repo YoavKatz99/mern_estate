fb71ec7359dbf4a3db9c962411ce965ab34db6bb
YoavKatz99 <yoav0123@gmail.com>
2024-01-02 12:11:51 +0200

    complete create lisitng page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import {Link} from "react-router-dom";
 import {
   getDownloadURL,
   getStorage,
@@ -14,7 +15,7 @@ import {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
-  signOutUserStart
+  signOutUserStart,
 } from "../redux/user/userSlice.js";
 export default function Profile() {
   const fileRef = useRef(null);
@@ -84,44 +85,41 @@ export default function Profile() {
       }
       dispatch(updateUserSuccess(data));
       setUpdateSuccess(true);
-
     } catch (error) {
       dispatch(updateUserFailure(error.message));
     }
   };
-const handleDeleteUser = async() =>{
-  try{
-    dispatch(deleteUserStart());
-    const res = await fetch(`api/user/delete/${currentUser._id}`, {
-      method: 'DELETE',
-    });
-    const data = await res.json();
-    if(data.success === false) {
-      dispatch(deleteUserFailure(data.message));
-      return;
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`api/user/delete/${currentUser._id}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(userDeleteFailure(error.message));
     }
-    dispatch(deleteUserSuccess(data));
-    }
-  catch(error){
-    dispatch(userDeleteFailure(error.message))
-  }
-};
+  };
 
-const handleSignOut = async () => {
-  try{
-    dispatch(signOutUserStart());
-    const res = await fetch('/api/auth/signout');
-    const data = await res.json();
-    if(data.success === false){
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch("/api/auth/signout");
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
       dispatch(deleteUserFailure(data.message));
-      return;
     }
-    dispatch(deleteUserSuccess(data));
-  }
-  catch(error){
-    dispatch(deleteUserFailure(data.message));
-  }
-}
+  };
 
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -179,17 +177,29 @@ const handleSignOut = async () => {
         ></input>
         <button
           disabled={loading}
-          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacty-95 disabled:opacity-80"
+          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80"
         >
           {loading ? "Loading..." : "Update"}
         </button>
+        <Link className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"  tp={"/create-listing"}>
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
-        <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete account</span>
-        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign out</span>
+        <span
+          onClick={handleDeleteUser}
+          className="text-red-700 cursor-pointer"
+        >
+          Delete account
+        </span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">
+          Sign out
+        </span>
       </div>
-      <p className="text-red-700 mt-5">{error ? error : ''}</p>
-      <p className="text-green-700 mt-5">{updateSuccess ? 'User is updated successfully!' : ''}</p> 
+      <p className="text-red-700 mt-5">{error ? error : ""}</p>
+      <p className="text-green-700 mt-5">
+        {updateSuccess ? "User is updated successfully!" : ""}
+      </p>
     </div>
   );
 }
