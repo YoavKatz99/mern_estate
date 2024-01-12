77a0c8263084d5620ade6ca94259a5bdddd82978
YoavKatz99 <yoav0123@gmail.com>
2024-01-12 13:31:36 +0200

    changed to Shrek Estate

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -29,7 +29,7 @@ export default function Header() {
         <Link to="/">
           {/*the flex will keep the header in a responsive way*/}
           <h1 className="font-bold text-sm sm: text-xl flex flex-wrap">
-            <span className="text-slate-500">Sanhad</span>
+            <span className="text-slate-500">Shrek</span>
             <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
