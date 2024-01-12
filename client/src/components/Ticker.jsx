c17b10ae22d237e057948556131d514011e7fdb6
YoavKatz99 <yoav0123@gmail.com>
2024-01-12 14:21:57 +0200

    include ticker

---
+++ client/src/components/Ticker.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+
+const Ticker = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "https://bringthemhomenow.net/1.0.8/hostages-ticker.js";
+    script.setAttribute(
+      "integrity",
+      "sha384-jQVW0E+wZK5Rv1fyN+b89m7cYY8txH4s3uShzHf1T51hdBTPo7yKL6Yizgr+Gp8C"
+    );
+    script.setAttribute("crossorigin", "anonymous");
+    document.getElementsByTagName("head")[0].appendChild(script);
+
+    // Cleanup the script when the component is unmounted
+    return () => {
+      document.getElementsByTagName("head")[0].removeChild(script);
+    };
+  }, []); // Empty dependency array ensures that this effect runs once when the component mounts
+
+  return (
+    <div className="hidden sm:block">
+      <div id="bthn" lang="en"></div>
+    </div>
+  );
+};
+
+export default Ticker;
